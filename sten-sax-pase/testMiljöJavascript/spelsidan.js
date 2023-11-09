48ae6e0f15195df5c7b565431490a7816823c4f9
LimpanGH <Linusj555@gmail.com>
2023-11-09 14:33:57 +0100

    linus and gabriel worked on sten sax påse-testmilö

--- sten-sax-pase/testMiljöJavascript/spelsidan.js
+++ sten-sax-pase/testMiljöJavascript/spelsidan.js
@@ -1,40 +1,103 @@
 /* Objective
-1 Get the user’s choice.
-2 Get the computer’s choice.
-3 Compare the two choices and determine a winner.
-4 Start the program and display the results. 
-5 Display image of player's choice and computer's choice
-6 Adjust scoreboard
+- Receive and display user name that we got from startsidan
+- Get the user’s choice.
+- Get the computer’s choice.
+- Compare the two choices and determine a winner.
+- Start the program and display the results. 
+- Display image of player's choice and computer's choice
+- Adjust scoreboard
+- Make winning dog move forward one step
+- When player or computer won 5 times: declare winner
 */
 
-
+// Receive and display user name that we got from startsidan
 const fullName = localStorage.getItem("fullName");
-document.getElementById("name").textContent = fullName;
-
-
-
-
-
-
-// Make user input equal to button click content
-const choice = document.querySelectorAll("#btn")
-const result = document.getElementById(choice)
-
-
-choice.forEach(button => button.addEventListener('click', () => {
+document.getElementById("name").textContent = fullName; // Make user input equal to button click content
+const choice = document.querySelectorAll("#btn");
+const result = document.getElementById(choice);
+choice.forEach((button) =>
+  button.addEventListener("click", () => {
     player = button.textContent;
-    result.textContent =`Result: ${player}`;
-  
-  
-  }));
+    result.textContent = `Result: ${player}`;
+  })
+);
 
+// MAKE rock-round/paper-round/scissors-round function
+//make a wincount function.
 
+let winPlayer = 0;
+let winComputer = 0;
+const roundResult = document.getElementById("result");
+const rock = document.getElementById("btnR");
+const paper = document.getElementById("btnP");
+const scissors = document.getElementById("btnS");
 
+const getComputerChoice = () => {
+  const computerInput = Math.floor(Math.random() * 3);
 
+  switch (computerInput) {
+    case 0:
+      return "ROCK";
+      break;
+    case 1:
+      return "PAPER";
+      break;
+    case 2:
+      return "SCISSORS";
+      break;
+  }
+};
 
- 
+function rockRound() {
+  let userInput = "ROCK";
+  let computerInput = getComputerChoice();
+  if (userInput === computerInput) {
+    roundResult.textContent = "DRAW";
+  } else if (computerInput === "SCISSORS") {
+    winPlayer++;
+    roundResult.textContent = "WIN";
+  } else {
+    winComputer++;
+    roundResult.textContent = "LOSE";
+  }
+  checkWinner();
+}
+function paperRound() {
+  let userInput = "PAPER";
+  let computerInput = getComputerChoice();
+  if (userInput === computerInput) {
+    roundResult.textContent = "DRAW";
+  } else if (computerInput === "ROCK") {
+    winPlayer++;
+    roundResult.textContent = "WIN";
+  } else {
+    winComputer++;
+    roundResult.textContent = "LOSE";
+  }
+  checkWinner();
+}
+function scissorsRound() {
+  let userInput = "SCISSORS";
+  let computerInput = getComputerChoice();
+  if (userInput === computerInput) {
+    roundResult.textContent = "DRAW";
+  } else if (computerInput === "PAPER") {
+    winPlayer++;
+    roundResult.textContent = "WIN";
+  } else {
+    winComputer++;
+    roundResult.textContent = "LOSE";
+  }
+}
+function checkWinner() {
+  if (winComputer == 5) {
+    roundResult.textContent = "You lost the game!";
+  } else if (winPlayer == 5) {
+    roundResult.textContent = "You won the game!";
+  }
+}
 
-
+// ------------------ ⬇️ Old parts that we might reuse ⬇️--------------------------
 
 // const getUserInput = (userInput) => {
 //   userInput = userInput.toUpperCase();
@@ -48,53 +111,53 @@ choice.forEach(button => button.addEventListener('click', () => {
 
 // console.log(getUserInput("rock"));
 
-const getComputerChoice = () => {
-  const computerChoice = Math.floor(Math.random() * 3);
+// const getComputerChoice = () => {
+//   const computerChoice = Math.floor(Math.random() * 3);
 
-  switch (computerChoice) {
-    case 0:
-      return "ROCK";
-      break;
-    case 1:
-      return "PAPER";
-      break;
-    case 2:
-      return "SCISSORS";
-      break;
-  }
-};
+//   switch (computerChoice) {
+//     case 0:
+//       return "ROCK";
+//       break;
+//     case 1:
+//       return "PAPER";
+//       break;
+//     case 2:
+//       return "SCISSORS";
+//       break;
+//   }
+// };
 
 // console.log(getComputerChoice());
 
-const winner = (userInput, computerChoice) => {
-  if (userInput == computerChoice) {
-    return "It/s a tie";
-  }
+// const winner = (userInput, computerChoice) => {
+//   if (userInput == computerChoice) {
+//     return "It/s a tie";
+//   }
 
-  if (userInput == "ROCK") {
-    if (computerChoice == "SCISSORS") {
-      return "DU";
-    } else {
-      return "Datorn";
-    }
-  }
+//   if (userInput == "ROCK") {
+//     if (computerChoice == "SCISSORS") {
+//       return "DU";
+//     } else {
+//       return "Datorn";
+//     }
+//   }
 
-  if (userInput == "PAPER") {
-    if (computerChoice == "ROCK") {
-      return "DU";
-    } else {
-      return "Datorn";
-    }
-  }
+//   if (userInput == "PAPER") {
+//     if (computerChoice == "ROCK") {
+//       return "DU";
+//     } else {
+//       return "Datorn";
+//     }
+//   }
 
-  if (userInput == "SCISSORS") {
-    if (computerChoice == "PAPER") {
-      return "DU";
-    } else {
-      return "Datorn";
-    }
-  }
-};
+//   if (userInput == "SCISSORS") {
+//     if (computerChoice == "PAPER") {
+//       return "DU";
+//     } else {
+//       return "Datorn";
+//     }
+//   }
+// };
 
 // console.log(determineWinner('SCISSORS', 'PAPER'));
 
@@ -108,22 +171,19 @@ const winner = (userInput, computerChoice) => {
 
 // runGame();
 
-const gameButtons = document.querySelectorAll("[data-selection]");
+// const gameButtons = document.querySelectorAll("[data-selection]");
 
-gameButtons.forEach((gameButtons) => {
-  gameButtons.addEventListener("click", (e) => {
-    const selectionName = gameButtons.dataset.selection;
-    makeSelection(selectionName);
-  });
-});
+// gameButtons.forEach((gameButtons) => {
+//   gameButtons.addEventListener("click", (e) => {
+//     const selectionName = gameButtons.dataset.selection;
+//     makeSelection(selectionName);
+//   });
+// });
 
-function makeSelection(selection) {
-  console.log(selection);
-}
+// function makeSelection(selection) {
+//   console.log(selection);
+// }
 
-const a = 10;
+// const a = 10;
 
-
-
-
-
+// ------------------ ⬆️ Old parts that we might reuse ⬆️--------------------------
--- sten-sax-pase/testMiljöJavascript/startsidan.js
+++ sten-sax-pase/testMiljöJavascript/startsidan.js
@@ -1,13 +1,57 @@
-const form = document.getElementById('form');
-const playerName = document.getElementById('name')
+// Objective
+// - Get the user’s name
+// - Send the name to spelsidan
 
-form.addEventListener('submit',(e) => {
-    e.preventDefault();
+// Getting user input and taking it over to spelsidan
+const form = document.getElementById("form");
+const playerName = document.getElementById("name");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const nameValue = playerName.value;
+  localStorage.setItem("fullName", nameValue);
+  window.location.href = "spelsidan.html";
+});
 
-    const nameValue = playerName.value;
+// Getting the chosen avatar and taking it over to spelsidan
+const body = document.body;
+const avatar = document.querySelectorAll(".avatar");
 
-    localStorage.setItem('fullName', nameValue)
+avatar.addEventListener("click", (getChosenAvatar));
 
-    window.location.href = 'spelsidan.html';
+function getChosenAvatar() {
+  switch (avatar) {
+    case avatar == ".avatar" && avatar == "#avatar 1":
+      //   return;
+      console.log("#avatar 1");
+      break;
+    case avatar == ".avatar" && avatar == "#avatar 2":
+      return;
+      "#avatar 2";
+      break;
+    case avatar == ".avatar" && avatar == "#avatar 3":
+      return;
+      "#avatar 3";
+      break;
+    case avatar == ".avatar" && avatar == "#avatar 4":
+      return;
+      "#avatar 4";
+      break;
+    case avatar == ".avatar" && avatar == "#avatar 5":
+      return;
+      "#avatar 5";
+      break;
+    case avatar == ".avatar" && avatar == "#avatar 6":
+      return;
+      "#avatar 6";
+      break;
+  }
+}
 
-})
+// const form = document.getElementById("form");
+// const playerName = document.getElementById("name");
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   const nameValue = playerName.value;
+//   localStorage.setItem("fullName", nameValue);
+//   window.location.href = "spelsidan.html";
+// });
