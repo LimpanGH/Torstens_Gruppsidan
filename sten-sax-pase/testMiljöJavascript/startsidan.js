8adef6c57f48dedf740cb109cf2877878e513af5
LimpanGH <Linusj555@gmail.com>
2023-11-19 10:58:55 +0100

    just in case

--- sten-sax-pase/testMiljöJavascript/startsidan.js
+++ sten-sax-pase/testMiljöJavascript/startsidan.js
@@ -1,8 +1,8 @@
 // Getting user input and taking it over to Spelsidan, fully working.
-const form = document.getElementById("form");
-const playerName = document.getElementById("name");
-// Appending a submit-event to form that fires when we click on the submit-button , and when an input is submittet, the fc runs .
-form.addEventListener("submit", (e) => {
+const form = document.getElementById('form');
+const playerName = document.getElementById('name');
+// Appending a submit-event to form that fires when we click on the submit-button, and when an input is submittet, the fc runs .
+form.addEventListener('submit', (e) => {
   // Prevents the default action of the submit-method which is submitting the form itself and refreshing the page.
   e.preventDefault();
   // .value is a property in JavaScript that retrieves the current value of an input field in a form, we store the value in a variable.
@@ -11,49 +11,49 @@ form.addEventListener("submit", (e) => {
   // The localStorage read-only property of the window interface allows you to access a Storage object for the Document's origin; the stored data is saved across browser sessions,
   // it gives you a set of methods to store, read, clear or delete items.
   // The setItem() method of the Storage interface takes two arguments, and when passed a key- name and value, will add that key to the given Storage object, or update that key's value if it already exists.
-  window.localStorage.setItem("fullName", nameValue);
+  window.localStorage.setItem('fullName', nameValue);
   // Redirects to our other page (spelsidan) when our form is submitted and the items have been set to localstorage
-  window.location.href = "spelsidan.html";
+  window.location.href = 'spelsidan.html';
 });
-//To check what is in your local storage: console.log(localStorage);
-
+//To check what is in your local storage:
+//console.log(localStorage);
 
 // Getting the chosen avatar and taking it over to spelsidan, not working yet.
-const avatar = document.querySelectorAll(".avatar");
+const avatar = document.querySelectorAll('.avatar');
 
 let switchFunction = () => {
   switch (true) {
-    case avatar.id == "#avatar1":
-      chosenAvatar = src = "img/charlesdeluvio-Mv9hjnEUHR4-unsplash.jpg";
+    case avatar.id == '#avatar1':
+      chosenAvatar = src = 'img/charlesdeluvio-Mv9hjnEUHR4-unsplash.jpg';
       return;
       chosenAvatar;
       break;
-    case avatar.id == "#avatar 2":
+    case avatar.id == '#avatar 2':
       return;
-      "#avatar 2";
+      '#avatar 2';
       break;
-    case avatar.id == "#avatar 3":
+    case avatar.id == '#avatar 3':
       return;
-      "#avatar 3";
+      '#avatar 3';
       break;
-    case avatar.id == "#avatar 4":
+    case avatar.id == '#avatar 4':
       return;
-      "#avatar 4";
+      '#avatar 4';
       break;
-    case avatar.id == "#avatar 5":
+    case avatar.id == '#avatar 5':
       return;
-      "#avatar 5";
+      '#avatar 5';
       break;
-    case avatar.id == "#avatar 6":
+    case avatar.id == '#avatar 6':
       return;
-      "#avatar 6";
+      '#avatar 6';
       break;
   }
-  window.localStorage.setItem("myAvatar", chosenAvatar);
-  window.location.href = "spelsidan.html";
+  window.localStorage.setItem('myAvatar', chosenAvatar);
+  window.location.href = 'spelsidan.html';
 };
 
-avatar.addEventListener("click", switchFunction);
+avatar.addEventListener('click', switchFunction);
 
 // const form = document.getElementById("form");
 // const playerName = document.getElementById("name");
