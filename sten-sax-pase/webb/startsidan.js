48f494b6a6d572c2956c0acb25a7f405d18e9d9d
LimpanGH <Linusj555@gmail.com>
2023-11-21 16:46:43 +0100

    merge now conflict free and working with javascript

--- sten-sax-pase/webb/startsidan.js
+++ sten-sax-pase/webb/startsidan.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 // ----- Getting user input and taking it over to Spelsidan. ------
 const form = document.querySelector('#form');
 const playerName = document.getElementById('playername');
-=======
-// Getting user input and taking it over to Spelsidan, fully working.
-const form = document.getElementById('form');
-const playerName = document.getElementById('name');
->>>>>>> master
+// console.log(playerName.value);
 // Appending a submit-event to form that fires when we click on the submit-button, and when an input is submittet, the fc runs .
 form.addEventListener('submit', (e) => {
   // Prevents the default action of the submit-method which is submitting the form itself and refreshing the page.
@@ -22,7 +17,6 @@ form.addEventListener('submit', (e) => {
   window.location.href = 'spelsidan.html';
 });
 //To check what is in your local storage:
-<<<<<<< HEAD
 // console.log(localStorage);
 
 // ------ Getting the chosen avatar and taking it over to spelsidan. ------
@@ -38,91 +32,3 @@ avatars.forEach((avatar) => {
     // window.location.href = 'spelsidan.html';
   });
 });
-
-// const gameButtons = document.querySelectorAll('[data-selection]');
-// gameButtons.forEach((gameButtons) => {
-//   gameButtons.addEventListener('click', (e) => {
-//     const selectionName = gameButtons.dataset.selection;
-//     makeSelection(selectionName);
-//   });
-// });
-// function makeSelection(selection) {
-//   console.log(selection);
-// }
-
-// What to do for  bark Audio on icon click:
-// 1. Get the element by class/id for icon buttons
-// 2. make a function that plays the Audio
-// 3. make the function run onclick
-// 4. connect the onclick to  appropriate html element
-
-// let bark = document.getElementsByClassName(avatar-flex);
-// element.addEventListener("click" function, (e) => {
-//  play
-
-//   )
-
-// onClick function for bark.mp3
-function playAudio(file) {
-  new Audio(file).play();
-}
-
-function borderClick(box) {
-  //selects all avatars as eligible items, and allows a max of 1 item to be affected by borderClick
-  var allAvatars = document.querySelectorAll('.avatar');
-  allAvatars.forEach(function (avatar) {
-    avatar.style.border = 'none';
-  });
-
-  //styles borders on click
-  box.style.border = '5px solid red';
-}
-=======
-//console.log(localStorage);
-
-// Getting the chosen avatar and taking it over to spelsidan, not working yet.
-const avatar = document.querySelectorAll('.avatar');
-
-let switchFunction = () => {
-  switch (true) {
-    case avatar.id == '#avatar1':
-      chosenAvatar = src = 'img/charlesdeluvio-Mv9hjnEUHR4-unsplash.jpg';
-      return;
-      chosenAvatar;
-      break;
-    case avatar.id == '#avatar 2':
-      return;
-      '#avatar 2';
-      break;
-    case avatar.id == '#avatar 3':
-      return;
-      '#avatar 3';
-      break;
-    case avatar.id == '#avatar 4':
-      return;
-      '#avatar 4';
-      break;
-    case avatar.id == '#avatar 5':
-      return;
-      '#avatar 5';
-      break;
-    case avatar.id == '#avatar 6':
-      return;
-      '#avatar 6';
-      break;
-  }
-  window.localStorage.setItem('myAvatar', chosenAvatar);
-  window.location.href = 'spelsidan.html';
-};
-
-avatar.addEventListener('click', switchFunction);
-
-// const form = document.getElementById("form");
-// const playerName = document.getElementById("name");
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   const nameValue = playerName.value;
-//   localStorage.setItem("fullName", nameValue);
-//   window.location.href = "spelsidan.html";
-// });
->>>>>>> master
